5356039aee49aa2f320caab69dca8ac671beb64c
George Tsiftsis <gtsiff@gmail.com>
2022-02-01 02:12:41 +0200

    Creation of Item

---
+++ src/Item/App.styles.ts
@@ -0,0 +1,9 @@
+import IconButton from "@material-ui/core/IconButton";
+import styled from "styled-components";
+
+export const StyledButton = styled(IconButton)`
+  position: fixed;
+  z-index: 100;
+  right: 20px;
+  top: 20px;
+`;
---
+++ src/Item/Item.tsx
@@ -0,0 +1,21 @@
+import Button from "@material-ui/core/Button";
+// Types
+
+import { CartItemType } from "../Types";
+import { Props } from "../Types";
+
+const Item: React.FC<Props> = ({ item, handleAddToCart }) => (
+  <div className="flex flex-col p-4 m-4 w-80 justify-center items-center  rounded-3xl border-2  ">
+    <img className="h-56 p-2  object-cover	" src={item.image} alt={item.title} />
+    <div>
+      <h3 className=" h-16 text-sm font-bold ">{item.title} </h3>
+      <h3 className="line-clamp-4  h-22 text-justify">{item.description}</h3>
+      <h3 className="py-4">${item.price}</h3>
+    </div>
+    <Button className="bg-blue-400" onClick={() => handleAddToCart(item)}>
+      Add to cart
+    </Button>
+  </div>
+);
+
+export default Item;
